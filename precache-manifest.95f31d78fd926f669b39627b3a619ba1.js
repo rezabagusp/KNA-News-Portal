902eccb3c0141ed828d80eb110da494cf8b48930
reza <reza@jojonomic.com>
2019-08-13 02:17:07 +0700

    Updates

---
+++ precache-manifest.95f31d78fd926f669b39627b3a619ba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db021cec48d27de55f419846c81f3aa2",
+    "url": "/KNA-News-Portal/index.html"
+  },
+  {
+    "revision": "336c405459aa7f11cf14",
+    "url": "/KNA-News-Portal/static/css/main.c316cff0.chunk.css"
+  },
+  {
+    "revision": "fb1dab100877654b3b1f",
+    "url": "/KNA-News-Portal/static/js/2.2f30adbd.chunk.js"
+  },
+  {
+    "revision": "336c405459aa7f11cf14",
+    "url": "/KNA-News-Portal/static/js/main.43f9e3db.chunk.js"
+  },
+  {
+    "revision": "7cabea4f09cc661db5ad",
+    "url": "/KNA-News-Portal/static/js/runtime~main.53779f6b.js"
+  }
+]);
